6f35aff46e41409323c0726b681cf7e8aa7e5d94
12philip34 <philipnssien@gmail.com>
2022-05-17 14:00:35 +0100

    pushing cdnApp

---
+++ CdnTutor/index.js
@@ -0,0 +1,14 @@
+const Navbar = (
+  <nav className= 'nav'>
+    <h1>MyWebsite</h1>
+    <ul className='ul'>
+      <li>softWares</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+    </nav>      
+)
+
+ReactDOM.render(
+  Navbar
+, document.getElementById('root'));
